39720604758d871545489b0f6535f40fbbfbf1e9
Athos Palmer <eu@athospalmer.com.br>
2019-04-11 14:53:37 -0300

    Implement jump to character controller

--- Assets/Scripts/ThirdPersonCharacterController.cs
+++ Assets/Scripts/ThirdPersonCharacterController.cs
@@ -5,6 +5,18 @@ using UnityEngine;
 public class ThirdPersonCharacterController : MonoBehaviour
 {
     public float Speed = 10;
+    public float JumpForce = 7;
+    public LayerMask GroundLayers;
+
+    Rigidbody rb;
+    CapsuleCollider col;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        col = GetComponent<CapsuleCollider>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -20,5 +32,21 @@ public class ThirdPersonCharacterController : MonoBehaviour
 
         Vector3 playerMovement = new Vector3(hor, 0f, ver) * Speed * Time.deltaTime;
         transform.Translate(playerMovement, Space.Self);
+
+        if (IsGrounded() && Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
+        }
+    }
+
+    // Check if the character is touching the ground
+    private bool IsGrounded()
+    {
+        return Physics.CheckCapsule(
+            col.bounds.center,
+            new Vector3(col.bounds.center.x, col.bounds.min.y, col.bounds.center.z),
+            col.radius * 0.9f,
+            GroundLayers
+        );
     }
 }
