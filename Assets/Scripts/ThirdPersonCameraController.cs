e9304df4f25aaaa0238b30d82a18ca5b32685ceb
Athos Palmer <eu@athospalmer.com.br>
2019-04-11 14:12:33 -0300

    Initial commit
    Initial Scene setup
    
    Implement controller for 3rd person camera and character

---
+++ Assets/Scripts/ThirdPersonCameraController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCameraController : MonoBehaviour
+{
+    public float Sensitivity = 1f;
+    public Transform Target, Player;
+    float mouseX, mouseY;
+
+    Transform Obstruction;
+    float zoomSpeed = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void LateUpdate()
+    {
+        CamControl();
+        ViewObstructed();
+    }
+
+    void CamControl()
+    {
+        mouseX += Input.GetAxis("Mouse X") * Sensitivity;
+        mouseY -= Input.GetAxis("Mouse Y") * Sensitivity;
+
+        // Set boundaries to the Y axis to prevent going too high or too low
+        mouseY = Mathf.Clamp(mouseY, -35f, 60f);
+
+        transform.LookAt(Target);
+
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            Target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        }
+        else
+        {
+            Target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+            Player.rotation = Quaternion.Euler(0, mouseX, 0);
+        }
+    }
+
+    void ViewObstructed()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, Target.position - transform.position, out hit, 4.5f))
+        {
+            if (hit.collider.gameObject.tag != "Player")
+            {
+                Obstruction = hit.transform;
+                Obstruction.gameObject.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
+
+                if (
+                    Vector3.Distance(Obstruction.position, transform.position) >= 3f &&
+                    Vector3.Distance(transform.position, Target.position) >= 1.5f
+                )
+                {
+                    transform.Translate(Vector3.forward * zoomSpeed * Time.deltaTime);
+                }
+            }
+            else
+            {
+                if (Obstruction) {
+                    Obstruction.gameObject.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+                }
+
+                if (Vector3.Distance(transform.position, Target.position) < 4.5f)
+                {
+                    transform.Translate(Vector3.back * zoomSpeed * Time.deltaTime);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ThirdPersonCharacterController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCharacterController : MonoBehaviour
+{
+    public float Speed = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    // Calculates the player movement by its inputs.
+    void PlayerMovement()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+
+        Vector3 playerMovement = new Vector3(hor, 0f, ver) * Speed * Time.deltaTime;
+        transform.Translate(playerMovement, Space.Self);
+    }
+}
