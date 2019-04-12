c691c190b143166ea28b67b7418c69022549ec67
Athos Palmer <eu@athospalmer.com.br>
2019-04-12 15:47:34 -0300

    Implement PlayerController
    Add Collision check to bounce from enemies when colliding
    
    Add Rigidbody on enemy prefab

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision col)
+    {
+        float force = 3;
+        if (col.gameObject.tag == "Enemy")
+        {
+            print(gameObject.name + " just hit " + col.gameObject.name);
+            
+            Vector3 direction = transform.position - col.transform.position;
+            direction.Normalize();
+
+            gameObject.GetComponent<Rigidbody>().AddForce(direction * force, ForceMode.Impulse);
+        }
+    }
+}
